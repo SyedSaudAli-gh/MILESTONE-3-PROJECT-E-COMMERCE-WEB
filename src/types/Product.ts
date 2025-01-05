3d6b0ec6e239092bdaece870d054aad08742b881
SyedSaudAli-gh <saud.saleem93@gmail.com>
2025-01-05 17:10:45 +0500

    update

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD:src/types/Product.ts
 export  interface Product {
     id: number;
     name: string;
     price: number;
     imageUrl: string;
   }
-=======
-export  interface Product {
-    id: number;
-    name: string;
-    price: number;
-    imageUrl: string;
-  }
->>>>>>> d0d4bb749273d3d792614b0bf9b0b9bc20094f0b:src/pages/Product.ts
   
